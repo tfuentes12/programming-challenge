b9288d05f4f6a5585369ec69eb44f5d4365ad0c7
tfuentes12 <tahiri.fuentes@gmail.com>
2017-01-26 19:06:46 -0400

    button disabling and name/email/message validation

---
+++ material-start/app/src/users/UserController.js
@@ -0,0 +1,47 @@
+var app = angular.module("users")
+  .controller('UserController', function(userService, $mdSidenav, $mdBottomSheet, $timeout, $log, $scope, $mdDialog, $location, $rootScope, $q) {
+
+    $scope.toggleLeft = buildDelayedToggler('left');
+
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+    
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+    // Hide or Show the 'left' sideNav area
+    function toggleUsersList() {
+      $mdSidenav('left').toggle();
+    }
+
+
+  })
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+
+    };
+
+});
